8bf7f13b3b13fb6dd743dd2c8dcef13acd0d424e
Henrique Andrade <henrique.ccda@gmail.com>
2022-06-23 14:51:56 -0300

    Trabalhando com propriedades.

--- projeto1/react-basico/src/components/Comentario.js
+++ projeto1/react-basico/src/components/Comentario.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './Comentario.css'
 
 
-//JSX
-const Comentario = () => (
+const Comentario = props => {
+  return (
     <div className='Comentario'>
-        <div>João</div>
-        <div>Olá, tudo bem?</div>
+      <h2>{props.nome}</h2>
+      <p>{props.email}</p>
+      <p>{props.children}</p>
+      <p>{props.data.toString()}</p>
     </div>
-);
+  )
+};
 
 export default Comentario;
