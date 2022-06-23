1c5b67694e833ad85fbc0b14fd3ba1c9f12421de
Henrique Andrade <henrique.ccda@gmail.com>
2022-06-23 14:01:27 -0300

    Comentario.

---
+++ projeto1/react-basico/src/components/Comentario.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Comentario.css'
+
+
+//JSX
+const Comentario = () => (
+    <div className='Comentario'>
+        <div>João</div>
+        <div>Olá, tudo bem?</div>
+    </div>
+);
+
+export default Comentario;
