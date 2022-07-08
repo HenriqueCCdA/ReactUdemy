074069280e764c6cf7552e0074a2992c0cb41e38
Henrique Andrade <henrique.ccda@gmail.com>
2022-07-08 19:24:31 -0300

    Conectando a Store.

--- projeto6/frutaria/src/components/ListaFrutas.jsx
+++ projeto6/frutaria/src/components/ListaFrutas.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import AdicionaFruta from './AdicionaFruta';
 import Fruta from './Fruta';
 
+
 const ListaFrutas = () => {
 
-  const frutas = [
-    { id: 1, nome: 'Abacaxi', quantidade: 5},
-    { id: 2, nome: 'Maçã', quantidade: 12}
-  ];
+  const frutas = useSelector(state => state.frutaReducers.frutas);
 
   return (
     <>
